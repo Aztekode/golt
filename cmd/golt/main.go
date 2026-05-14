3bd13967a584f18e8b012f24966cf2274dd00ff4
Eddy Ortega <atrox390@gmail.com>
2026-05-14 12:37:47 -0700

    fix: version bad number

--- cmd/golt/main.go
+++ cmd/golt/main.go
@@ -27,7 +27,7 @@ func main() {
 		Use:     "golt",
 		Short:   "Golt Runtime - TS/JS Backend Engine",
 		Long:    fmt.Sprintf("%s[Golt] Runtime%s\nA blazing fast backend engine to run TypeScript/JavaScript directly on Go.", Cyan, Reset),
-		Version: "1.0.2",
+		Version: "1.0.1",
 	}
 
 	var initCmd = &cobra.Command{
